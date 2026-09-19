41bb295ced62b7d6129c4d8641ebfc4375fada2d
IEVN1001-23000818 <menavillegaskj@gmail.com>
2026-09-19 10:03:02 -0600

    Pipes 19/09/2026

--- src/app/heroes/heroes-list/heroes-list.ts
+++ src/app/heroes/heroes-list/heroes-list.ts
@@ -1,8 +1,64 @@
 import { Component } from '@angular/core';
+import { IHeroes } from '../heroes'; /* shift+alt+. para importar automaticamente lo que requiero */ 
+
 
 @Component({
   selector: 'app-heroes-list',
   standalone: false,
   templateUrl: './heroes-list.html',
 })
-export class HeroesList {}
+export class HeroesList {
+
+imageWidth:number=40;
+ imageMargin:number=2; 
+  muestraImage:boolean=true;
+  listFilter:string='';
+  
+  showImage():void{
+    this.muestraImage=!this.muestraImage;
+  }
+  
+  heroes:IHeroes[]=[
+
+
+    {
+      imagen:"https://dragonball-api.com/characters/goku_normal.webp",
+      nombre:"Goku",
+      description:"El protagonista de la serie",
+      race:"Saiyan",
+      ki:60000
+
+    },
+
+    {
+      imagen:"https://dragonball-api.com/characters/vegeta_normal.webp",
+      nombre:"Vegeta",
+      description:"Príncipe de los Saiyans",
+      race:"Saiyan",
+      ki:54000
+
+    },
+    {
+      imagen:"https://dragonball-api.com/characters/bulma.webp",
+      nombre:"Bulma",
+      description:"Bulma es la protagonista femenina",
+      race:"Human",
+      ki:0
+
+    },
+    {
+      imagen:"https://dragonball-api.com/characters/gohan.webp",
+      nombre:"Gohan",
+      description:"Simplemente Gohan en Hispanoamérica",
+      race:"Saiyan",
+      ki:45000
+
+    }
+
+
+  ]
+
+
+
+
+}
---
+++ src/app/heroes/heroes.ts
@@ -0,0 +1,11 @@
+export interface IHeroes { /*Poner una I para indicar que es una interfaz*/
+
+    imagen:string;
+      nombre:string;
+      description:string;
+      race:string;
+      ki:number
+
+
+
+} 
